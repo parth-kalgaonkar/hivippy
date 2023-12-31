c1e0300fdbc61643e7e64d643cadd2d95f65e58a
Parth Kalgaonkar <parthkalgaonkar@gmail.com>
2023-12-31 15:38:36 +0530

    Fixed index.html  path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === "production" ? "/hivippy/" : "/",
+	base: process.env.NODE_ENV === "production" ? "/hivippy/" : "./",
   plugins: [
     vue(),
   ],
